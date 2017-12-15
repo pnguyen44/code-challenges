28fd2aa1d121b2c9fd2baf68b981d288a071faf6
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-12-15 14:04:09 -0500

    Rename file and add description about function

--- coding-challenges/add-binary.js
+++ coding-challenges/add-binary.js
@@ -1,3 +1,12 @@
+// write a function that takes two integer as an argument and return the sum of the integers as a binary number.
+
+function addBinary(a,b){
+  return (a+b).toString(2)
+}
+
+console.log(addBinary(10, 3))
+
+// alternative solution
 // function addBinary(a,b) {
 // let quotient
 // let dec = a + b
@@ -10,8 +19,3 @@
 //   }
 //   return remainderStr.split('').reverse().join('')
 // }
-function addBinary(a,b){
-  return (a+b).toString(2)
-}
-
-console.log(addBinary(10, 3))
