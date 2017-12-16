bf50b271e76b4801c860cfbc7042f3761f7530d2
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-12-15 21:28:02 -0500

    Add findChildren function   - A function that take a string with upper and lower case letter. It   returns the sorted string with upper case letter appearing first   before it lower case versions.

---
+++ coding-challenges/findChildren.js
@@ -0,0 +1,28 @@
+// 6 kyu - Where is my parent!?(cry)
+// Mothers arranged dance party for children in school.On that party there are only
+// mothers and their children.All are having great fun on dancing floor when suddenly all lights went out.Its dark night and no one can see eachother.But you were flying nearby and you can see in the dark and have ability to teleport people anywhere you want.
+//
+// Legend:
+// -Uppercase letters stands for mothers,lowercase stand for their children. I.E "A" mothers children are "aaaa".
+// -Function input:String contain only letters,Uppercase letters are unique.
+// Task:
+// Place all people in alphabetical order where Mothers are followed by their children.I.E "aAbaBb" => "AaaBbb".
+
+function findChildren(dancingBrigade){
+  let char = ''
+   return dancingBrigade.toLowerCase().split('').sort().map(x => {
+    if(x !== char) {
+      char = x
+      return x.toUpperCase()
+    } else {
+      return x
+    }
+  }).join('')
+};
+
+// alternative solution:
+// function findChildren(dancingBrigade){
+//   return dancingBrigade.split('').sort((a, b) => {
+//     return a.localeCompare(b, 'kf', {caseFirst: 'upper'})
+//   }).join('')
+// };
