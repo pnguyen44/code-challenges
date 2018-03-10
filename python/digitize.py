73c41f99b57ca1cba30f7142c5400f9447b4cc8c
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-03-09 22:03:15 -0500

    Refactor code

--- python/digitize.py
+++ python/digitize.py
@@ -7,9 +7,8 @@
 # Example: 348597 => [7,9,5,8,4,3]
 
 def digitize(n):
-    n = str(n)[::-1]
     answer = []
-    for x in n:
+    for x in str(n)[::-1]:
         answer.append(int(x))
     return answer
 
@@ -18,4 +17,4 @@ def digitize(n):
 #     return map(int, str(n)[::-1])
 #
 # def digitize(n):
-#     return [int(x) for x in str(n)[::-1]]    
+#     return [int(x) for x in str(n)[::-1]]
