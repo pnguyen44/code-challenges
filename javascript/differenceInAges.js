540008d701baffc6a1ec4316c63f4d39ba8a10e4
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-06 02:23:32 -0400

    Add differenceInAges function    - A function that takes an array of numbers and Return a new array    with [youngest age, oldest age, difference between the youngest and    oldest age] # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is ahead of 'origin/master' by 9 commits. #   (use "git push" to publish your local commits) # # Changes to be committed: #	modified:   differenceInAges.js #

--- javascript/differenceInAges.js
+++ javascript/differenceInAges.js
@@ -2,3 +2,9 @@
 // At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
 //
 // You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
+
+function differenceInAges(ages){
+  ages = ages.sort((a,b) => a - b)
+  let diff = ages[ages.length - 1] - ages[0]
+  return [ages[0], ages[ages.length - 1], diff];
+}
