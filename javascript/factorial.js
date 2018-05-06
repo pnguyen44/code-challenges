b025fc24c236f87e0deaa243c7299986b6b2030d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-06 00:29:40 -0400

    Refactor code

--- javascript/factorial.js
+++ javascript/factorial.js
@@ -13,11 +13,7 @@
 
 
 const factorial = n => {
-  if (n ===0) {
-    return 1;
-  } else {
-    return n * factorial(n-1)
-  }
+  return  n === 0 ? 1 : n * factorial(n-1)
 };
 
 let answer =  factorial(2)
