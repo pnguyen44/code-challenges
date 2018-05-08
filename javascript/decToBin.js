7ce4eea515dfba2405563b611376fb4dd032e93a
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-08 08:27:43 -0400

    Add decToBin function   - A function that convert decimal numbers to binary.

--- javascript/decToBin.js
+++ javascript/decToBin.js
@@ -1,3 +1,14 @@
-// Decimal to binary converter 
+// Decimal to binary converter
 //
 // Convert decimal numbers to binary
+
+function decToBin(d) {
+  let res = []
+  let x = d
+  do {
+    res.push(x)
+    d = x
+    x = Math.floor(d/2)
+  } while (x !== 0)
+  return res.reverse().map(x => x === 0 ? 0 : x % 2 === 0 ? 0 : 1).join('')
+}
