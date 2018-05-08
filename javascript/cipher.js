05e9890fbe7a48d61d48cb63e85345736042e24b
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-08 06:42:56 -0400

    Add encode and decode function   -Encode function create a cipher. The cipher involves taking each   character of a string and multiplying their char codes by 6.

--- javascript/cipher.js
+++ javascript/cipher.js
@@ -6,3 +6,19 @@
 // You must write two functions:
 // encode to encode a given string,
 // decode to decode a given string.
+
+
+function encode(str) {
+  return str.split('').map( (c,i,a )=> {
+    return String.fromCharCode(str.charCodeAt(i) * 6)
+  }).join('')
+}
+
+function decode(str) {
+  return str.split('').map( (c,i,a )=> {
+    return String.fromCharCode(str.charCodeAt(i) / 6)
+  }).join('')
+}
+
+let answer =  decode("ưɞʈʈʚÀȊʚʬʈɘÆ")
+console.log(answer)
