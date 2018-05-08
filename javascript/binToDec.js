6d3040344918843bd612c16546878c7cc54ef419
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-08 08:40:36 -0400

    Add binToDec function    - A function which converts a binary number (given as a string) to a    decimal number.

--- javascript/binToDec.js
+++ javascript/binToDec.js
@@ -1,2 +1,6 @@
 // 8 kyu - Bin to Decimal
 // Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin){
+  return [...bin].reverse().map((e,i) => e * Math.pow(2,i)).reduce((a,c) => a+c)
+}
