e41aa3b876fe6394fabbb772c3d024fae238ebf0
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-08 06:20:39 -0400

    Correct spelling error

--- javascript/splitter.js
+++ javascript/splitter.js
@@ -1,5 +1,5 @@
 // 7 kyu - Split camelcase
-// Split a camelcase string into individual words, the return value must be a single string of words seporated by one whitespace.
+// Split a camelcase string into individual words, the return value must be a single string of words separated by one whitespace.
 //
 // The strings are to be split on the capital letters like so:
 //
