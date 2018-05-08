8c9a4bb22102e0835eb4888ddf0fa9a64764ab7a
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-08 06:18:06 -0400

    Add code challenge

---
+++ javascript/splitter.js
@@ -0,0 +1,6 @@
+// 7 kyu - Split camelcase
+// Split a camelcase string into individual words, the return value must be a single string of words seporated by one whitespace.
+//
+// The strings are to be split on the capital letters like so:
+//
+// 'StringStringString' => 'String String String'
