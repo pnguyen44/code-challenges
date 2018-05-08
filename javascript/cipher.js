2f1262eb3113ded08d8c46eb35274f071f8b044f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-08 06:42:08 -0400

    Add code challenge

---
+++ javascript/cipher.js
@@ -0,0 +1,8 @@
+// 7 kyu - Cipher
+// This cipher involves taking each character of a string and multiplying their char codes by 6.
+//
+// For example, Hello World! would become ưɞʈʈʚÀȊʚʬʈɘÆ.
+//
+// You must write two functions:
+// encode to encode a given string,
+// decode to decode a given string.
