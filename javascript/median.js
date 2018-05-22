f2bc32ae25b147c5337d557c733b42d7bdee09b2
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-22 15:20:31 -0400

    Add median function   - A function that finds the median of an array.

--- javascript/median.js
+++ javascript/median.js
@@ -10,3 +10,12 @@
 // Input [1, 2, 3, 4] --> Median 2.5
 //
 // Input [3, 4, 1, 2, 5] --> Median 3
+
+function median(n){
+  n = n.sort((a,b) => a - b)
+  if (n < 1 ) return false;
+  return n.length % 2 ? n[Math.floor(n.length / 2)] : (n[n.length / 2 - 1]  + n[n.length / 2]) / 2
+}
+
+let answer =  median([ 26, 38, 71, 52, 61, 46, 28, 64, 94, 83, 38, 5, 28, 38, 45 ])
+console.log(answer)
