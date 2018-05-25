ea5eec73d03443d3ab032d6e9f3de954b48bc06f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-25 18:03:32 -0400

    Add iccanobif function   - A function that creates an array of size n with the values of the   Fibonnaci sequence, and reverse the order in which the sequence is   displayed.

--- javascript/iccanobif.js
+++ javascript/iccanobif.js
@@ -4,3 +4,16 @@
 // For example: [1, 1, 2, 3, 5] would become [5, 3, 2, 1, 1]
 //
 // You can assume that n will always be a positive integer between and including, 1 and 64.
+
+
+function iccanobif(n) {
+  if (n ===1) return [1]
+  let arr = [1,1]
+  for (let i = 0; i < n - 2; i++) {
+    arr.push(arr[i] + arr[i + 1])
+  }
+  return arr.reverse()
+}
+
+let answer = iccanobif(3)
+console.log(answer)
