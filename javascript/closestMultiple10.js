8fe0ccdf2da372e8c0fced7f6e104d362d1d1380
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-25 17:17:52 -0400

    Add alternative solution

--- javascript/closestMultiple10.js
+++ javascript/closestMultiple10.js
@@ -16,3 +16,11 @@ const closestMultiple10 = num => {
   let str = Math.floor(num).toString()
   return str[str.length -1] >= 5 ? (Math.floor(num/10) + 1) * 10 : Math.floor(num/10) * 10
 };
+
+// Alternative Solution:
+// const closestMultiple10 = num => {
+//   return Math.round(num/10)*10;
+// };
+
+let answer =  closestMultiple10(2015.8251288536699)
+console.log(answer)
