685e2dee5d30e869653b80e9734357f5a7f8a45b
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-06-12 23:11:45 -0400

    Add code challenge

--- javascript/factorial.js
+++ javascript/factorial.js
@@ -1,20 +1,4 @@
-// 7 kyu - Recursion #1 - Factorial
-// ##1 - Factorial
+// 7 kyu - Factorial
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
 //
-// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
-//
-// 5! = 5 * 4 * 3 * 2 * 1 = 120.
-//
-// The value of 0! is 1.
-//
-// #Your task
-//
-// You have to create the function factorial that receives n and returns n!. You have to use recursion.
-
-
-const factorial = n => {
-  return  n === 0 ? 1 : n * factorial(n-1)
-};
-
-let answer =  factorial(2)
-console.log(answer)
+// Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript).
