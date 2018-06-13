659b51b6c1b9b20f3bcba2f3c45d3cf6bdb4a2f5
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-06-12 21:55:17 -0400

    Add sumArr function   - A function that takes 2 arrays of 5 string numbers each, and outputs   the sum of the corresponding elements as strings as well.

--- javascript/sumArr.js
+++ javascript/sumArr.js
@@ -4,3 +4,15 @@
 //   sumArr(['4','5','6','7','8'],['1','2','3','4','5']) // => ['5','7','9','11','13']
 //   sumArr(['34','5','200','17','6'],['27','24','14','90','16']) // => ['61','29','214','107','22']
 // If any input is an empty string, it should return the number that isn't just an empty string. If both corresponding elements are empty, output "0".
+
+
+function sumArr(a,b) {
+  let res = []
+  for(let i = 0; i < a.length; i++) {
+    res.push((Number(a[i]) + Number(b[i])).toString())
+  }
+  return res
+}
+
+let answer = sumArr(['4','5','6','7','8'],['1','2','3','4','5'])
+console.log(answer)
