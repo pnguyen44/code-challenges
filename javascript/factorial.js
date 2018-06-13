9b33b2abfa73a31a84481d4bf113abb2963b173b
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-06-12 23:15:58 -0400

    Add alternative solution

--- javascript/factorial.js
+++ javascript/factorial.js
@@ -15,5 +15,11 @@ function factorial(n){
   return res
 }
 
+// Alternative Solution:
+// function factorial(n){
+//   if(n < 0 || n > 12 )  throw new RangeError()
+//   return n <= 1 ? 1 : n * factorial(n-1)
+// }
+
 let answer = factorial(3)
 console.log(answer)
