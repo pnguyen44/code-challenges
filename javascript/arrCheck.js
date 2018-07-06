127f50cf990d6f0d8058e45c4bd5da5f62de7ea4
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-06 17:32:17 -0400

    Add arrCheck function   - A function that checks if every value in the array an array.

--- javascript/arrCheck.js
+++ javascript/arrCheck.js
@@ -8,3 +8,12 @@
 // [[1],[2]] => true
 // ['1','2'] => false
 // [{1:1},{2:2}] => false
+
+
+const arrCheck = value => {
+  return value.every(x => Array.isArray(x))
+}
+
+
+let answer = arrCheck(['A', 'R', 'R', 'A', 'Y'])
+console.log(answer)
