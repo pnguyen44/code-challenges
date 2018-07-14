90cddbc5d5f0cb4fc5ce1b4342125bedb3b7caf5
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-14 02:29:34 -0400

    Add specialSort function   - A function that takes an array of integers and changes the order of   the integers so negative numbers appear first then positive numbers.

--- javascript/PN/specialSort.js
+++ javascript/PN/specialSort.js
@@ -1,2 +1,11 @@
 // Given an array of integers, write a function that changes the order of the integers so
 // negative numbers appear first then positive numbers. Do not change the order of the integers.  
+
+function specialSort(num) {
+  let neg = num.filter(x => x < 0)
+  let pos = num.filter(x => x >= 0)
+  return neg.concat(pos)
+}
+
+let test = specialSort([-7, -9, 9, -4, 0, 1])
+console.log(test)
