71fd2316692a3096a5d9f5d36fbc378c23eb6b18
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-14 02:16:06 -0400

    Change code challenge to be case sensitive

--- javascript/PN/checkString.js
+++ javascript/PN/checkString.js
@@ -1,4 +1,4 @@
-// Write a function to determine if a string is a substring of another string.  
+// Write a function to determine if a case sensitive string is a substring of another string.  
 
 function checkString(str, word) {
   return str.split(' ').includes(word)
