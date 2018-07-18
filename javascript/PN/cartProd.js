34c74c51b1bb2887ad95b3e260a8736521ccd320
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-18 02:39:05 -0400

    Add cartProd2Sets function   - A function that returns the cartesian product of two sets.

--- javascript/PN/cartProd.js
+++ javascript/PN/cartProd.js
@@ -1,1 +1,28 @@
 // Write a function that returns the cartesian product of two sets as an array.
+
+function cartProd2Sets(setA, setB) {
+  let result = []
+  let set = []
+  setA.forEach(x => {
+    for (let i = 0; i < setB.length; i++) {
+      set = [x, setB[i]]
+      result.push(set)
+      set = []
+    }
+  })
+  return result
+}
+
+// test
+const a = [1]
+const b = [1, 2]
+const c = [1, 2, 3]
+const x = ['a']
+const y = ['a', 'b']
+const z = ['a', 'b', 'c']
+
+console.log(cartProd2Sets(a,a))
+console.log(cartProd2Sets(a,x))
+console.log(cartProd2Sets(x,a))
+console.log(cartProd2Sets(c,x))
+console.log(cartProd2Sets(b,y))
