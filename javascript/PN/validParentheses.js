67adf63c48e9e8026f59e704a7f98c73c95ee52d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-26 17:13:35 -0400

    Add alternative solution

--- javascript/PN/validParentheses.js
+++ javascript/PN/validParentheses.js
@@ -24,3 +24,15 @@ function validParentheses(parens){
   }
   return stack.length === 0
 }
+
+
+// Alternative Solution:
+// function validParentheses(parens){
+//   var indent = 0;
+//
+//   for (var i = 0 ; i < parens.length && indent >= 0; i++) {
+//     indent += (parens[i] == '(') ? 1 : -1;
+//   }
+//
+//   return (indent == 0);
+// }
