4a25a6fb309dc68476456e5074ff65958b76005c
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-28 15:11:54 -0400

    Refactor code

--- javascript/PN/minJumps.js
+++ javascript/PN/minJumps.js
@@ -4,22 +4,24 @@
 
 
 function minJumps(arr) {
-  let numJumps = [0]
-  let actualJumps = []
-  if (arr[0] === 0) return 0
+  let numJumps = []
+  if (arr[0] === 0) return Infinity
+
+  numJumps[0] = 0
+
   for (let i = 1; i < arr.length; i ++) {
+    numJumps[i] = Infinity
     for (let j = 0; j < i; j++) {
-      if (arr[j] + j >=  i) {
-        numJumps.push(numJumps[j] + 1)
-        actualJumps.push(j)
+      if (arr[j] + j >=  i && numJumps[j] !== Infinity) {
+        numJumps[i] = Math.min(numJumps[i],numJumps[j] + 1)
         break
       }
     }
   }
-  return numJumps[numJumps.length - 2]
+  return numJumps[numJumps.length - 1]
 }
 
 //test
 // console.log(minJumps([1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9])) // 3 (1-> 3 -> 8 ->9)
 // console.log(minJumps([2,3,1,1,2,4,2,0,1,1])) // 4 (2-> 3 -> 2 -> 4 -> 1)
-console.log(minJumps([0,8,3,6,9,3,0,0,1,3])) // 2 (2-> 8 -> 3)
+console.log(minJumps([2,8,3,6,9,3,0,0,1,3])) // 2 (2-> 8 -> 3)
