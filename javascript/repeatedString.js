33be35bf439f00f19d4efb5c6f2b49dc7674462c
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-10-18 19:18:00 -0400

    Update code challenge   - add test cases

--- javascript/repeatedString.js
+++ javascript/repeatedString.js
@@ -1,11 +1,18 @@
-Lilah has a string,s, of lowercase English letters that she repeated infinitely many times.
+// Lilah has a string,s, of lowercase English letters that she repeated infinitely many times.
+//
+// Given an integer,n, find and print the number of letter a's in the first n letters of Lilah's infinite string.
+//
+//
+// Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length n in the infinitely repeating string.
+//
+// repeatedString has the following parameter(s):
+//
+// s: a string to repeat
+// n: the number of characters to consider
 
-Given an integer,n, find and print the number of letter a's in the first n letters of Lilah's infinite string.
+function repeatedString(s, n) {
 
+}
 
-Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length n in the infinitely repeating string.
-
-repeatedString has the following parameter(s):
-
-s: a string to repeat
-n: the number of characters to consider
+console.log(repeatedString('abc', 10)) // 7
+console.log(repeatedString('a', 1000000000000)) // 1000000000000
