e2d8cbd1f7c41be7f2544633b24abffd7911104d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-10-18 19:18:56 -0400

    Add repeatedString function   - A function that takes a string and an integer and return an integer   representing the number of occurrences of a in the prefix of length n   in the infinitely repeating string.

--- javascript/repeatedString.js
+++ javascript/repeatedString.js
@@ -11,6 +11,21 @@
 // n: the number of characters to consider
 
 function repeatedString(s, n) {
+    if (s === 'a') return n;
+    const matchStr = s.match(/a/g)
+    if (matchStr === null) return 0;
+    const numWholeStr = Math.floor(n / s.length)
+    // let str = s.repeat(numWholeStr)
+    let result = numWholeStr * matchStr.length
+    let str = s.slice(0,n - (numWholeStr * s.length))
+    str = str.match(/a/g)
+    if (str === null) {
+        return result
+    } else {
+        return result + str.length
+    }
+    // console.log(str)
+    // return str.match(/a/g).length;
 
 }
 
