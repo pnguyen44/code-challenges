621066f617eb7b6fc239db3af5d458031ea308b3
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-10-25 23:17:17 -0400

    Add alternative solution

--- javascript/makeAnagram.js
+++ javascript/makeAnagram.js
@@ -1,22 +1,22 @@
 // Complete the makeAnagram function in the editor below. It must return an integer representing the minimum total characters that must be deleted to make the strings anagrams.
 
 
-function makeAnagram(a, b) {
-  const shortLong = a.length < b.length ? [a,b] : [b,a];
-  let shortW = shortLong[0];
-  const longW = shortLong[1]
-  let count = 0;
-  shortW = shortW.split('');
-  for (let i = 0; i < longW.length; i++) {
-    const pos = shortW.indexOf(longW[i])
-    if (pos === -1) {
-      count +=1;
-    } else {
-      shortW.splice(pos,1)
-    }
-  }
-  return count + shortW.length;
-}
+// function makeAnagram(a, b) {
+//   const shortLong = a.length < b.length ? [a,b] : [b,a];
+//   let shortW = shortLong[0];
+//   const longW = shortLong[1]
+//   let count = 0;
+//   shortW = shortW.split('');
+//   for (let i = 0; i < longW.length; i++) {
+//     const pos = shortW.indexOf(longW[i])
+//     if (pos === -1) {
+//       count +=1;
+//     } else {
+//       shortW.splice(pos,1)
+//     }
+//   }
+//   return count + shortW.length;
+// }
 
 
 // Alternative Solution:
@@ -37,6 +37,29 @@ function makeAnagram(a, b) {
 //   })
 //   return a.length + b.length - matches * 2
 // }
+
+function makeAnagram(a, b) {
+  const count = new Array(26).fill(0);
+  const pos = s => {
+    return s.charCodeAt(0) - 'a'.charCodeAt(0);
+  }
+
+  for (let i = 0; i < a.length; ++i) {
+    count[pos(a.charAt(i))]++;
+  }
+
+  for (let i = 0; i < b.length; ++i) {
+    count[pos(b.charAt(i))]--;
+  }
+
+  // console.log(count);
+
+  const diff = count.reduce((p, c) => {
+    return Math.abs(p) + Math.abs(c);
+  }, 0);
+
+  return diff;
+}
 
 
 console.log(makeAnagram("cde", "abc")); // 4
