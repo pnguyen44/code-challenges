080d79b6ec1de9a4a64c640e1cb01fc6146db6f7
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-10-25 23:19:25 -0400

    Edit grammar error

--- javascript/makeAnagram.js
+++ javascript/makeAnagram.js
@@ -1,25 +1,24 @@
 // Complete the makeAnagram function in the editor below. It must return an integer representing the minimum total characters that must be deleted to make the strings anagrams.
 
 
-// function makeAnagram(a, b) {
-//   const shortLong = a.length < b.length ? [a,b] : [b,a];
-//   let shortW = shortLong[0];
-//   const longW = shortLong[1]
-//   let count = 0;
-//   shortW = shortW.split('');
-//   for (let i = 0; i < longW.length; i++) {
-//     const pos = shortW.indexOf(longW[i])
-//     if (pos === -1) {
-//       count +=1;
-//     } else {
-//       shortW.splice(pos,1)
-//     }
-//   }
-//   return count + shortW.length;
-// }
+function makeAnagram(a, b) {
+  const shortLong = a.length < b.length ? [a,b] : [b,a];
+  let shortW = shortLong[0];
+  const longW = shortLong[1]
+  let count = 0;
+  shortW = shortW.split('');
+  for (let i = 0; i < longW.length; i++) {
+    const pos = shortW.indexOf(longW[i])
+    if (pos === -1) {
+      count +=1;
+    } else {
+      shortW.splice(pos,1)
+    }
+  }
+  return count + shortW.length;
+}
 
-
-// Alternative Solution:
+// Alternative Solutions:
 // function makeAnagram(a, b) {
 //   a = a.split("");
 //   b = b.split("");
@@ -38,25 +37,25 @@
 //   return a.length + b.length - matches * 2
 // }
 
-function makeAnagram(a, b) {
-  const count = new Array(26).fill(0);
-  const pos = s => {
-    return s.charCodeAt(0) - 'a'.charCodeAt(0);
-  }
-
-  for (let i = 0; i < a.length; ++i) {
-    count[pos(a.charAt(i))]++;
-  }
-
-  for (let i = 0; i < b.length; ++i) {
-    count[pos(b.charAt(i))]--;
-  }
-  const diff = count.reduce((p, c) => {
-    return Math.abs(p) + Math.abs(c);
-  }, 0);
-
-  return diff;
-}
+// function makeAnagram(a, b) {
+//   const count = new Array(26).fill(0);
+//   const pos = s => {
+//     return s.charCodeAt(0) - 'a'.charCodeAt(0);
+//   }
+//
+//   for (let i = 0; i < a.length; ++i) {
+//     count[pos(a.charAt(i))]++;
+//   }
+//
+//   for (let i = 0; i < b.length; ++i) {
+//     count[pos(b.charAt(i))]--;
+//   }
+//   const diff = count.reduce((p, c) => {
+//     return Math.abs(p) + Math.abs(c);
+//   }, 0);
+//
+//   return diff;
+// }
 
 
 // console.log(makeAnagram("cde", "abc")); // 4
