b1ff8937a342d415b01651b2fe37b72112677f28
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-10-25 22:50:13 -0400

    Update code challenge   - Add test cases

--- javascript/makeAnagram.js
+++ javascript/makeAnagram.js
@@ -1,1 +1,5 @@
 // Complete the makeAnagram function in the editor below. It must return an integer representing the minimum total characters that must be deleted to make the strings anagrams.
+
+
+// console.log(makeAnagram("cde", "abc")); // 4
+// console.log(makeAnagram("showman", "woman")); // 2`
