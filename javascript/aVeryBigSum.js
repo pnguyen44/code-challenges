9a84e94293e3c6419b8a7865ca29e9465dfb29a2
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-11-21 20:22:51 -0500

    Update code challenge   - add a test case

--- javascript/aVeryBigSum.js
+++ javascript/aVeryBigSum.js
@@ -1,2 +1,11 @@
 // Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
 // Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+
+
+function aVeryBigSum($ar) {
+
+
+}
+
+console.log(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]))
