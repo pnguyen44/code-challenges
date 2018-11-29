6f218e1c2c51bf6bfbac5d2caa52ef7a29f1a157
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-11-28 22:34:33 -0500

    Clean code

--- javascript/plusMinus.js
+++ javascript/plusMinus.js
@@ -9,45 +9,44 @@
 // 0.400000
 // 0.200000
 
-// function plusMinus(arr) {
-//   let countPos = 0
-//   let count0 = 0
-//   let countNeg = 0
-//   arr.forEach(function (e) {
-//       if (e === 0) {
-//           count0 += 1
-//       } else if (e > 0) {
-//           countPos += 1
-//       } else {
-//           countNeg += 1
-//       }
-//   })
-//
-//   console.log((countPos / arr.length).toFixed(6))
-//   console.log((countNeg / arr.length).toFixed(6))
-//   console.log((count0 / arr.length).toFixed(6))
-// }
+function plusMinus(arr) {
+  let countPos = 0
+  let count0 = 0
+  let countNeg = 0
+  arr.forEach(function (e) {
+      if (e === 0) {
+          count0 += 1
+      } else if (e > 0) {
+          countPos += 1
+      } else {
+          countNeg += 1
+      }
+  })
+
+  console.log((countPos / arr.length).toFixed(6))
+  console.log((countNeg / arr.length).toFixed(6))
+  console.log((count0 / arr.length).toFixed(6))
+}
 
 // Alternative solution
-function plusMinus(arr) {
-  let pos = 0
-  let neg = 0
-  let zero = 0
-
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i])
-    if (arr[i] > 0) {
-      pos++
-    } else if (arr[i] < 0) {
-      neg++
-    } else {
-      zero++
-    }
-  }
-    console.log((pos/ arr.length).toFixed(6))
-    console.log((neg / arr.length).toFixed(6))
-    console.log((zero / arr.length).toFixed(6))
-}
+// function plusMinus(arr) {
+//   let pos = 0
+//   let neg = 0
+//   let zero = 0
+//
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       pos++
+//     } else if (arr[i] < 0) {
+//       neg++
+//     } else {
+//       zero++
+//     }
+//   }
+//     console.log((pos/ arr.length).toFixed(6))
+//     console.log((neg / arr.length).toFixed(6))
+//     console.log((zero / arr.length).toFixed(6))
+// }
 
 
 console.log(plusMinus([-4, 3, -9, 0, 4, 1]))
