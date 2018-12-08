11e2b21b77c0ee06cb3be7ba10d647002bc8b93a
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-12-07 23:01:16 -0500

    Add staircase function   - solution to code challenge

--- javascript/staircase.js
+++ javascript/staircase.js
@@ -13,6 +13,33 @@
 
 
 function staircase(n) {
+    // var n = parseInt(readLine());
+    var arr = []
+    var s = 0
+    var space = 0
+    var hash = 0
+    for(let l=1; l<=n; l++){
+       space = n - l
+        hash = n - space
+
+        for(s=1; s<=space; s++){
+            arr.push(' ')
+        }
+
+          for(let h=1; h<=hash; h++){
+             arr.push("#")
+
+          }
+
+
+                // console.log(arr)
+
+            // arr.push(space)
+    // arr.push(hash)
+    console.log(arr.join(''))
+
+        arr = []
+    }
 }
 
 
