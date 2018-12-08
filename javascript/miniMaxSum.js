d38eeb3a61bd027ba93579d26fccf4c477fa1b53
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-12-08 00:56:16 -0500

    Add miniMaxSum function   - solution to min-max sum code challenge

--- javascript/miniMaxSum.js
+++ javascript/miniMaxSum.js
@@ -6,7 +6,15 @@
 // Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
 function miniMaxSum(arr) {
-
+    arr = arr.sort((a,b) => a - b);
+    let maxSum = arr.slice(1).reduce((acc,curr) => {
+      return acc + curr;
+    });
+    let minSum = arr.slice(0,arr.length - 1).reduce((acc,curr) => {
+      return acc + curr;
+    });
+    // console.log(maxSum)
+    console.log(minSum + ' ' + maxSum)
 }
 
 console.log(miniMaxSum([1, 2, 3, 4, 5])) // 10 14
