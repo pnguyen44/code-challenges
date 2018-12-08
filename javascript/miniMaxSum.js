dc10f09fc1fe1bc70a7119116ceba2f27a92110e
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-12-08 00:53:23 -0500

    Update code challenge   - add test cases

--- javascript/miniMaxSum.js
+++ javascript/miniMaxSum.js
@@ -1,4 +1,13 @@
+'use strict';
+
 // Mini-Max Sum
 
 // Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers.
 // Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+function miniMaxSum(arr) {
+
+}
+
+console.log(miniMaxSum([1, 2, 3, 4, 5])) // 10 14
+// console.log(miniMaxSum([7, 69, 2, 221, 8974])) // 299 9271
