41c5430811367014a475e6e5df05422700e82d7d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-12-07 22:54:30 -0500

    Update code challenge   - Add a test case

--- javascript/staircase.js
+++ javascript/staircase.js
@@ -1,4 +1,3 @@
-
 //Staircase
 // Consider a staircase of size n = 4 :
 //
@@ -11,3 +10,16 @@
 // Observe that its base and height are both equal to n, and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
 //
 // Write a program that prints a staircase of size n.
+
+
+function staircase(n) {
+}
+
+
+console.log(staircase(6))
+//      #
+//     ##
+//    ###
+//   ####
+//  #####
+// ######
