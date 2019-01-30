869b33e6b0f6da8b2a753748d3a690f5bf051e57
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-01-30 01:43:30 -0500

    Update code challenge   - add test cases

--- javascript/class.js
+++ javascript/class.js
@@ -6,10 +6,12 @@
 
 
 
+
+
  const rectangle = new Polygon([10, 20, 10, 20]);
  const square = new Polygon([10, 10, 10, 10]);
  const pentagon = new Polygon([10, 20, 30, 40, 43]);
 
- console.log(rectangle.perimeter());
- console.log(square.perimeter());
- console.log(pentagon.perimeter());
+ console.log(rectangle.perimeter()); // 60
+ console.log(square.perimeter()); //40
+ console.log(pentagon.perimeter()); //143
