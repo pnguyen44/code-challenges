85d12bdc380e5481a87a9a2e2b5ddc91b174e1e7
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-01-30 01:43:59 -0500

    Add solution to class code challenge

--- javascript/class.js
+++ javascript/class.js
@@ -4,9 +4,16 @@
  * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
  */
 
-
-
-
+ class Polygon {
+     constructor(arr) {
+         this.arr = arr;
+     };
+     perimeter() {
+        return this.arr.reduce(function (acc, curr) {
+             return acc + curr;
+         })
+     }
+ }
 
  const rectangle = new Polygon([10, 20, 10, 20]);
  const square = new Polygon([10, 10, 10, 10]);
