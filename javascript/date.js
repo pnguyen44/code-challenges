1e430fd4a744c72122133ed78fa8f963953ca521
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-01-30 22:52:55 -0500

    Add test case for code challenge

--- javascript/date.js
+++ javascript/date.js
@@ -1,1 +1,10 @@
-// write a function that takes 2 string dates and return the differnce in days between the 2 dates. 
+// write a function that takes 2 string dates and return the differnce in days between the 2 dates.
+
+
+
+function getDateDifference(d1,d2) {
+
+
+}
+
+getDateDifference('12/01/2019','12/1/2018') // 365
