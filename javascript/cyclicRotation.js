11aa64d1dc076830f03b452c15b4dcf804959a99
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-02-01 03:48:08 -0500

    Update code challenge with test case

--- javascript/cyclicRotation.js
+++ javascript/cyclicRotation.js
@@ -1,4 +1,5 @@
-// An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
+// An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index,
+//nd the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
 //
 // The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
 //
@@ -25,3 +26,13 @@
 //
 // N and K are integers within the range [0..100];
 // each element of array A is an integer within the range [−1,000..1,000].
+
+
+function solution(A, K) {
+
+}
+
+let A = [3, 8, 9, 7, 6]
+let K = 1
+
+ console.log(solution(A,K)) // [6, 3, 8, 9, 7]
