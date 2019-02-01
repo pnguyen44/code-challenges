dc66086523e5969148de88848bcb85285d053a28
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-02-01 00:44:04 -0500

    Add comment to code

--- javascript/PN/minJumps.js
+++ javascript/PN/minJumps.js
@@ -8,16 +8,24 @@ function minJumps(arr) {
   if (arr[0] === 0) return Infinity
 
   numOfJumps[0] = 0
-
+// Start at the first element, no jumps needed to get there.
   for (let i = 1; i < arr.length; i ++) {
+
+    // Set the current element to a super high number so that any number of jumps would be less than this super high number
     numOfJumps[i] = Infinity
+
+     // loop through the previous elements
     for (let j = 0; j < i; j++) {
       if (arr[j] + j >=  i && numOfJumps[j] !== Infinity) {
+
+         // we choose the smaller number of either numOfJumps[i] (what the current min number of jumps to get to element [i]) or
+         // numOfJumps[j]+1 (what the min number of jumps is to get to element [j] plus one more jump)
         numOfJumps[i] = Math.min(numOfJumps[i],numOfJumps[j] + 1)
       }
     }
   }
   return numOfJumps[numOfJumps.length - 1]
+  // reach the number of jumps it took to get to the last element 
 }
 
 // Given an array of integers where each element represents the max number of steps that can be made forward from that element.
