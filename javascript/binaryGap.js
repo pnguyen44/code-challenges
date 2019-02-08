5d55c3cf56023a1a31b859e8c0ccca541d062009
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-02-08 04:30:25 -0500

    Update binary gap code challenge   - add test cases

--- javascript/binaryGap.js
+++ javascript/binaryGap.js
@@ -2,3 +2,12 @@
 //
 // For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and
 // contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+
+
+
+function solution(N) {
+}
+
+
+console.log(solution(  32 )) // 0
+console.log(solution(  1041 )) // 5
