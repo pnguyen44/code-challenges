1632c2a3b8c416bb10818157278f340c253b3d1f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-02-10 03:06:46 -0500

    Add solution to PermMissingElem code challenge

--- javascript/permMissingElem.js
+++ javascript/permMissingElem.js
@@ -24,10 +24,29 @@
 // each element of array A is an integer within the range [1..(N + 1)].
 
 function solution(A) {
-
+  // write your code in JavaScript (Node.js 8.9.4)
+  if (A.length === 0) return 1
+  A.sort((a,b) => a - b)
+  let missing
+  for(let i = 0; i < A.length -1; i++){
+      if (A[i] - A[i + 1] !== -1){
+          missing = A[i] + 1
+          break;
+      }
+  }
+  // console.log(missing)
+  if (missing) {
+      return missing
+  } else {
+      if (A[0] === 1) {
+          return A[A.length - 1] + 1
+      } else {
+          return A[0] - 1
+      }
+  }
 }
 
 
 console.log(solution([])) //1
-console.log(solution([2]])) //1
+console.log(solution([2])) //1
 console.log(solution([2,3,1,5])) //4
