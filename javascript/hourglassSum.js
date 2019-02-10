b5797019176804e1855fe29956ddc20fe4e64641
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-02-10 04:11:17 -0500

    Update hourglassSum code challenge

--- javascript/hourglassSum.js
+++ javascript/hourglassSum.js
@@ -47,3 +47,28 @@
 // arr: an array of integers
 //
 // Print the largest (maximum) hourglass sum found in arr.
+
+
+
+
+function hourglassSum(arr) {
+
+
+}
+
+
+// console.log(hourglassSum(
+// [[1, 1, 1, 0, 0, 0],
+// [0, 1, 0, 0, 0, 0],
+// [1, 1, 1, 0, 0, 0],
+// [0, 0, 2, 4, 4, 0],
+// [0, 0, 0, 2, 0, 0],
+// [0, 0, 1, 2, 4, 0 ]])) // 19
+
+console.log(hourglassSum(
+  [[-1, -1, 0 ,-9 ,-2 , -2],
+  [-2, -1, -6, -8, -2, -5],
+  [-1, -1, -1, -2, -3, -4],
+  [-1, -9, -2, -4, -4, -5],
+  [-7, -3, -3, -2, -9, -9],
+  [-1, -3, -1, -2, -4, -5]])) // -6
