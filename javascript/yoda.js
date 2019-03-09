a3412ac118139bdec658c655824b7fd741630a45
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-09 03:07:17 -0500

    Add solution to yoda code challenge

--- javascript/yoda.js
+++ javascript/yoda.js
@@ -1,6 +1,16 @@
 // Give you a phrase and a number, write a funcaton that take the number of words from the phrase and move them to the back
 
+function yoda(phrase, N) {
+  phrase = phrase.split(' ')
 
+  const end = phrase.slice(0,N).join(' ')
+  const start = phrase.slice(N).join(' ')
+  console.log(end)
+  console.log(start)
+
+  return `${start} ${end}`
+
+}
 
 
 console.log(yoda('one two three four', 2)) // three four one two
