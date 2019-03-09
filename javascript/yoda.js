3b2031fa6061afe84a48969683ff1832c704331d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-09 03:06:40 -0500

    Add test case

--- javascript/yoda.js
+++ javascript/yoda.js
@@ -1,1 +1,6 @@
 // Give you a phrase and a number, write a funcaton that take the number of words from the phrase and move them to the back
+
+
+
+
+console.log(yoda('one two three four', 2)) // three four one two
