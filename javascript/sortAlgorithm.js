316d7e147abb62ca82a9b4bbabb6c98ce3450020
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-09 03:12:31 -0500

    Add test case

--- javascript/sortAlgorithm.js
+++ javascript/sortAlgorithm.js
@@ -1,1 +1,9 @@
 // write a function that preform a binary search
+function binarySearchRecursive(arr, x, left = 0, right = arr.length - 1) {
+
+}
+
+
+let A = [2, 5, 1, 3, 7, 2, 3, 8, 6, 3]
+console.log(binarySearchRecursive(A, 7)) // true
+console.log(binarySearchRecursive(A, 0)) // false
