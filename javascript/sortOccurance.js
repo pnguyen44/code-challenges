60664d91fb9ea20ae83b2de22a626f776452603c
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-14 23:46:23 -0400

    Add solution to sort occurrence code challenge

--- javascript/sortOccurance.js
+++ javascript/sortOccurance.js
@@ -1,9 +1,35 @@
 // write a function takes a string, sorts the characters in string by the number of occurances in accending order
-//, and returns that stirng
+//, and returns that stirng.
 
-function sortOccurance(str) {
+function sortOccurence(str) {
+  let obj = str.split('').reduce((obj,val) => {
+    if (obj[val]) {
+      obj[val] += obj[val]
+    } else {
+      obj[val] = 1
+    }
+    return obj
+  },{})
+  console.log('obj=', obj)
+  let sortedKey = Object.keys(obj).sort((a,b) => {
+    if (obj[a] === obj[b]) {
+      if (a < b) {
+        return - 1
+      } else {
+        return 1
+      }
+    } else {
+      return obj[a] - obj[b]
+    }
+  })
+  console.log('sortedKey', sortedKey)
+  let ans = ""
+  for (let letter of sortedKey) {
+    ans += letter.repeat(obj[letter])
+  }
+  return ans
 
 }
 
 
-// console.log(sortOccurance('ccdbbaa')) // daabbcc
+console.log(sortOccurence('ccdbbaa')) // daabbcc
