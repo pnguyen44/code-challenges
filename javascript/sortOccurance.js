2a42eb14624fac9b875ff650b12bd7acb3b7c3a7
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-14 23:44:10 -0400

    Add sort occurrence code challenge

---
+++ javascript/sortOccurance.js
@@ -0,0 +1,9 @@
+// write a function takes a string, sorts the characters in string by the number of occurances in accending order
+//, and returns that stirng
+
+function sortOccurance(str) {
+
+}
+
+
+// console.log(sortOccurance('ccdbbaa')) // daabbcc
