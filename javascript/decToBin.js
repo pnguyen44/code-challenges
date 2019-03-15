94a503ccd1cef31cafcfce1638954d38eb7e1a50
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-14 23:36:39 -0400

    Add alternative solution

--- javascript/decToBin.js
+++ javascript/decToBin.js
@@ -2,16 +2,29 @@
 //
 // Convert decimal numbers to binary
 
+// function decToBin(d) {
+//   let res = []
+//   let x = d
+//   do {
+//     res.push(x)
+//     d = x
+//     x = Math.floor(d/2)
+//   } while (x !== 0)
+//   return res.reverse().map(x => x === 0 ? 0 : x % 2 === 0 ? 0 : 1).join('')
+// }
+
+// Alternative Solution:
 function decToBin(d) {
-  let res = []
-  let x = d
-  do {
-    res.push(x)
-    d = x
-    x = Math.floor(d/2)
-  } while (x !== 0)
-  return res.reverse().map(x => x === 0 ? 0 : x % 2 === 0 ? 0 : 1).join('')
+  let bin = ''
+  while(d > 0) {
+    bin += d % 2
+    d = Math.trunc(d / 2)
+  }
+  // console.log(bin)
+  return bin.split('').reverse().join('')
 }
+
+
 
 console.log(decToBin(10)) // 1010
 console.log(decToBin(14)) // 1110
