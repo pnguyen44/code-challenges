6e1a964f58ff952d9890219e06faef9664dc761a
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-18 16:19:44 -0400

    Add test cases

--- javascript/pangrams.js
+++ javascript/pangrams.js
@@ -1,1 +1,15 @@
 // given a sentence, find out if it contains all letters from a-z. Hint: they are specifically looking for set.
+
+
+
+function pangrams(str) {
+
+}
+
+
+console.log(pangrams('qmExzBIJmdELxyOFWv LOCmefk TwPhargKSPEqSxzveiun')) // pangram
+console.log(pangrams('We promptly judged antique ivory buckles for the next prize')) // pangram
+console.log(pangrams('Two driven jocks help fax my big quiz' ))  // pangram
+console.log(pangrams('Jackdaws love my big sphinx of quartz')) // pangram
+console.log(pangrams('We promptly judged antique ivory buckles for the prize')) // not pangram
+console.log(pangrams('Johnathon quickly extemporized five tow bag')) // not pangram
