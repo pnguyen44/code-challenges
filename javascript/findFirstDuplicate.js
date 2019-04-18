fa1e3a0f6b8225a2f3b95db4c2d3738e76abde80
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-18 17:01:23 -0400

    Add test cases

--- javascript/findFirstDuplicate.js
+++ javascript/findFirstDuplicate.js
@@ -1,1 +1,14 @@
 // Write a function that receives an array of integers, and return the first occurrence of a duplicate.
+
+
+function findFirstDuplicate(arr) {
+
+}
+
+
+// console.log(findFirstDuplicate([10, 5, 3, 4, 3, 5, 6])) // 5
+// console.log(findFirstDuplicate([-10, -5, 6, 3, 4, 3, 5, 6])) // 6
+// console.log(findFirstDuplicate([-10])) // []
+// console.log(findFirstDuplicate([1, 8989, 8998, 9889, 8, 9, -8998, 89, 8989])) // 8989
+// console.log(findFirstDuplicate([4, -4, 5, 0, 2, 12, 20])) // []
+// console.log(findFirstDuplicate([33, 3, 333, 0.3, -3, -33, 333])) // 333
