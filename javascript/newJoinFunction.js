1a02ad7ab71273e391551c2e33ceb496e523802d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-18 17:03:14 -0400

    Add test cases

--- javascript/newJoinFunction.js
+++ javascript/newJoinFunction.js
@@ -1,1 +1,11 @@
 // Write a function that works like the javascript .join() method.
+
+function newJoinFunction(arr, separator=',') {
+
+}
+
+
+// console.log(newJoinFunction(['Fire', 'Wind', 'Rain'])) // Fire,Wind,Rain
+// console.log(newJoinFunction(['Fire', 'Wind', 'Rain'],'')) // FireWindRain
+// console.log(newJoinFunction(['Fire', 'Wind', 'Rain'],'-')) // Fire-Wind-Rain
+// console.log(newJoinFunction(['cat', 'Dog', 'apple'], 'hi')) // cathiDoghiapple
