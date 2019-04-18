5e04c2233f2a169704a4733054fb84f041f7011a
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-18 16:57:44 -0400

    Add test cases

--- javascript/areaOfCircle.js
+++ javascript/areaOfCircle.js
@@ -1,1 +1,15 @@
 // Given an input of a radius, return the area of a circle.
+
+
+function areaOfCircle(radius) {
+
+
+}
+
+// console.log(areaOfCircle(8.292)) //  216.01
+// console.log(areaOfCircle(34.97)) // 3841.86
+// console.log(areaOfCircle(5.15))  // 83.32
+// console.log(areaOfCircle(0.04)) // 5.03×10(power -3)
+// console.log(areaOfCircle(12223432)) // 4.69×1014
+// console.log(areaOfCircle( 423)) // 5.62×105
+// console.log(areaOfCircle(0.00343)) //3.63×10-5
