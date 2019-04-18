9ee810c9210f296a4ca02ba2a91b2964ba6cdfcb
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-18 16:23:50 -0400

    Add timeConversion function   - solution to time conversion code challenge

--- javascript/timeConversion.js
+++ javascript/timeConversion.js
@@ -2,8 +2,22 @@
 // Give a string input with a time format of '5:30 PM', change the input to military time.
 
 
+const getHour = (time, isPM) => {
+  if(isPM) {
+    return (time % 12) + 12
+  } else {
+    time = (time % 12)
+    return `0${time}`
+  }
+}
 function timeConversion(s) {
-
+  // remvoe PM and AM
+  const strWithoutLetters = s.slice(0, s.length - 2)
+  const isPM = s.slice(-2).toUpperCase() === 'PM'
+  const strNumArr = strWithoutLetters.split(':')
+  const hour = getHour(strNumArr[0],isPM)
+  const result = `${hour}:${strNumArr[1]}:${strNumArr[2]}`
+  return result
 }
 
 // console.log(timeConversion('07:05:45PM')) // 19:05:45
