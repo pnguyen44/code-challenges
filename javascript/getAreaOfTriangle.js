de61600451de6dcfa782b67d88dcd1dc840c94ab
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-18 16:59:45 -0400

    Add test cases

--- javascript/getAreaOfTriangle.js
+++ javascript/getAreaOfTriangle.js
@@ -1,1 +1,12 @@
 // Given a base and a height return the area of a triangle.
+
+function getAreaOfTriangle(base, height) {
+
+}
+
+// console.log(getAreaOfTriangle(3,4)) // 6
+// console.log(getAreaOfTriangle(23,4)) // 46
+// console.log(getAreaOfTriangle(4,23)) // 46
+// console.log(getAreaOfTriangle(44,463)) // 10186
+// console.log(getAreaOfTriangle(77,89)) // 34576.5
+// console.log(getAreaOfTriangle(0.23, 0.97)) // 0.11
