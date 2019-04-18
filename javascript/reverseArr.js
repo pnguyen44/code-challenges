c8d45800a11047a367ca3b3f5e013bc70b03cf97
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-18 17:05:14 -0400

    Add test cases

--- javascript/reverseArr.js
+++ javascript/reverseArr.js
@@ -1,1 +1,14 @@
 // Reverse an array without using the built in reverse functionality.
+
+function reverseArr(arr) {
+
+}
+
+
+// console.log(reverseArr([3,4,10,1])) // [1,10,4,3]
+// console.log(reverseArr([16, 13, 7, 2, 1, 12])) // [12, 1, 2, 7, 13, 16]
+// console.log(reverseArr([3])) // [3]
+// console.log(reverseArr([])) // []
+// console.log(reverseArr([3,3, 4, 4, 3,0])) // [0, 3, 4,4, 3,3]
+// console.log(reverseArr( [3, -3, -4, -7,34, 22, 0, -2])) // [-2, 0, 22, 4, -7, -4, -3, 3]
+// console.log(reverseArr([6, 5, -6])) // [-6, 5, 6]
