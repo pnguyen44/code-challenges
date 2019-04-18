15b5b803787c0de2b42e5a45ad18de102649a445
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-18 16:23:18 -0400

    Add test cases

--- javascript/timeConversion.js
+++ javascript/timeConversion.js
@@ -1,2 +1,14 @@
 // Write a function that converts a string input of standard time to military time.
 // Give a string input with a time format of '5:30 PM', change the input to military time.
+
+
+function timeConversion(s) {
+
+}
+
+// console.log(timeConversion('07:05:45PM')) // 19:05:45
+// console.log(timeConversion('12:40:22AM')) // 00:40:22
+// console.log(timeConversion('06:40:03AM')) //  06:40:03
+// console.log(timeConversion('01:10:10PM')) //  13:10:10
+// console.log(timeConversion('03:33:15AM')) // 03:33:15
+// console.log(timeConversion('05:15:55PM')) // 17:15:55
