edb20c43a215738335c8ae465ff51597a464e0cb
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-19 18:35:29 -0400

    Add test cases

--- javascript/PN/findTotalNumOfPairs.js
+++ javascript/PN/findTotalNumOfPairs.js
@@ -1,1 +1,18 @@
 // Given n people, find total number of pairs can be formed.  1 Answer - They are looking for recursion.
+
+
+
+
+// console.log(findTotalNumOfPairs(1)) //  0
+// console.log(findTotalNumOfPairs(2)) // 1
+// console.log(findTotalNumOfPairs(3)) // 3
+// console.log(findTotalNumOfPairs(4)) // 6
+// console.log(findTotalNumOfPairs(5)) // 10
+// console.log(findTotalNumOfPairs(6)) // 15
+// console.log(findTotalNumOfPairs(7)) // 21
+// console.log(findTotalNumOfPairs(8)) // 28
+// console.log(findTotalNumOfPairs(9)) // 36
+console.log(findTotalNumOfPairs(10)) // 45
+console.log(findTotalNumOfPairs(564)) // 158766
+// console.log(findTotalNumOfPairs(44)) // 946
+console.log(findTotalNumOfPairs(9853)) // 48535878
