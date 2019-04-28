bb43d56b11c31536b41a96e54525d35694577a09
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-28 17:23:33 -0400

    Add test inputs and outputs

--- javascript/MinStack.js
+++ javascript/MinStack.js
@@ -6,3 +6,14 @@
 // pop() -- Removes the element on top of the stack.
 // top() -- Get the top element.
 // getMin() -- Retrieve the minimum element in the stack.
+
+
+
+let minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+console.log(minStack.getMin());  //  -3
+minStack.pop();
+console.log(minStack.top());      // 0
+console.log(minStack.getMin());   // -2
