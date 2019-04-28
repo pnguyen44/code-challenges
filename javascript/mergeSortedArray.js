baca944437e2d5d9a96ae6dc6ced13f49bce457f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-28 16:32:44 -0400

    Add test input and output

--- javascript/mergeSortedArray.js
+++ javascript/mergeSortedArray.js
@@ -5,3 +5,6 @@
 // The number of elements initialized in nums1 and nums2 are m and n respectively.
 // You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
 //
+
+
+console.log(merge( [1,2,3,0,0,0] , 3 , [2,5,6] , 3)) // [1,2,2,3,5,6]
