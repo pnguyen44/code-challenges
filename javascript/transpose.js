0b823a7cb781ac2d4a0d81512d1694b2d570d212
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-27 20:49:51 -0400

    Add solution to Transpose Matrix

--- javascript/transpose.js
+++ javascript/transpose.js
@@ -20,5 +20,23 @@
 // 1 <= A.length <= 1000
 // 1 <= A[0].length <= 1000
 
+var transpose = function(A) {
+    let result = []
+    for (let i = 0; i < A[0].length; i++) {
+        let current = A[i]
+        let newCombo = []
+        console.log('i..', i)
+        for (let j = 0; j < A.length; j++ ) {
+          console.log('j, i', j , i)
+          console.log('add', A[j][i])
+          newCombo.push(A[j][i])
+        }
+        console.log('newCombo', newCombo)
+        result.push(newCombo)
+    }
+    return result
+};
+
+
 // console.log(transpose([[1,2,3],[4,5,6],[7,8,9]])) // [[1,4,7],[2,5,8],[3,6,9]]
 console.log(transpose([[1,2,3],[4,5,6]])) // [[1,4],[2,5],[3,6]]
