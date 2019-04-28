3c7c47150588b4648262d4a526aac79a3382ff99
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-28 17:24:00 -0400

    Add code solution to MinStack

--- javascript/MinStack.js
+++ javascript/MinStack.js
@@ -7,6 +7,50 @@
 // top() -- Get the top element.
 // getMin() -- Retrieve the minimum element in the stack.
 
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    this.items = [];
+};
+
+/*
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+  this.items.push(x)
+};
+
+/*
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+  this.items.pop()
+};
+
+/*
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+  return this.items[this.items.length - 1]
+};
+
+/*
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+  return Math.min.apply(null, this.items )
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
 
 
 let minStack = new MinStack();
