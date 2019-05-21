bb8acbb05fc623bf512fd158e13c969bae12cfcb
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-05-21 16:55:10 -0400

    Add code solution to sockMerchant code challenge

--- javascript/sockMerchant.js
+++ javascript/sockMerchant.js
@@ -12,5 +12,22 @@
 // n: the number of socks in the pile
 // ar: the colors of each sock
 
+function sockMerchant(n, ar) {
+    let color = []
+    let numOfPairs = 0
+    for (let i = 0; i < n; i++) {
+        if (!color.includes(ar[i])) {
+          const counts = ar.filter(x => x === ar[i]).length
+          const pairsCount = Math.floor(counts/2)
+          console.log('pairs', pairsCount)
+          if (pairsCount > 0 ) {
+            numOfPairs+= pairsCount
+            color.push(ar[i])
+          }
+        }
+    }
+    return numOfPairs
+}
+
 
 console.log(sockMerchant( 9, [10, 20, 20, 10, 10, 30, 50, 10, 20])) // 3
