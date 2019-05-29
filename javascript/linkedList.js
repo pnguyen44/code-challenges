38bdf1a08c417dadc6566d323704d983d7ae2059
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-05-28 23:37:50 -0400

    Add insert method to LinkedList class

--- javascript/linkedList.js
+++ javascript/linkedList.js
@@ -14,5 +14,22 @@ class LinkedList {
     this.head = null,
     this.size = 0
   }
+  
+  insert(data) {
+    // create a new node
+    let node = new Node(data)
+    let current
+    if (this.head == null) {
+      this.head = node
+    } else {
+      current = this.head
+      while(current.next) {
+        current = current.next
+      }
+      // add node
+      current.next = node
+    }
+    this.size++
+  }
 
 }
