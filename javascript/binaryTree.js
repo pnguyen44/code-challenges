be5c3639c111438737ff9d7699d3e90be683bc0c
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-05-28 23:49:18 -0400

    Add Insert method to Node class

--- javascript/binaryTree.js
+++ javascript/binaryTree.js
@@ -8,6 +8,23 @@ class Node {
     this.right = null
   }
 
+  insert(value) {
+    // if value is less than the actuall data of our node, then insert it on the left
+    // else insert it to the right
+    if (value <= this.data) {
+      if (this.left == null) {
+        this.left = new Node(value)
+      } else {
+        this.left.insert(value)
+      }
+    } else {
+      if (this.right == null) {
+        this.rigth = new Node(value)
+      } else {
+        this.right.insert(value)
+      }
+    }
+  }
 
 
 }
