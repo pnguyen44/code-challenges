8c6643266bcc63adccf22a1048506cf6d2addc96
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-05-28 23:55:23 -0400

    Add contains method in Node class

--- javascript/binaryTree.js
+++ javascript/binaryTree.js
@@ -25,6 +25,22 @@ class Node {
       }
     }
   }
-
+  contains(value) {
+      if (value == this.data) {
+        return true
+      } else if (value < this.data) {
+        if (this.left == null) {
+          return false
+        } else {
+          return this.left.contains(value)
+        }
+      } else {
+          if (this.right == null) {
+            return false
+          } else {
+            return this.right.contains(value)
+          }
+      }
+  }
 
 }
