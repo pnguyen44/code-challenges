b9c2238c7524899991ee92c80836ce14f9e4d939
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-06-02 20:13:13 -0400

    Add alternative solution

--- javascript/singleNumber.js
+++ javascript/singleNumber.js
@@ -24,6 +24,18 @@
 //   }
 // }
 
+// // alternative solution
+function singleNumber(nums) {
+  const numbers = {}
+  for (let i = 0; i < nums.length; i++) {
+    if (!numbers[nums[i]]) {
+      numbers[nums[i]] = true
+    } else {
+      delete numbers[nums[i]]
+    }
+  }
+  return Object.keys(numbers)[0]
+}
 
 
 console.log(singleNumber([2,2,1])) // 1
