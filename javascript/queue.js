a0682af89d805822d21cb1f5e04ed3522f75a580
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-06-15 22:17:34 -0400

    Add PriorityQueue class

--- javascript/queue.js
+++ javascript/queue.js
@@ -35,16 +35,54 @@ class Queue {
   }
 }
 
+class PriorityQueue extends Queue {
+  constructor() {
+    super()
+  }
+  add(element) {
+    if (this.isEmpty()) {
+      this.items.push(element)
+    } else {
+      let added = false
+      for(let i = 0; i < this.items.length; i++) {
+        // console.log(this.items[i])
+        if (element[1] < this.items[i,1]) {
+          // collection.splice(i,0,element);
+          console.log('got here')
+          this.items.splice(i, 0, element)
+          added = true
+          break
+        }
+      }
+      if (!added) {
+        this.items.push(element)
+      }
+    }
+  }
+}
+
+
 const queue = new Queue()
 
-queue.add(10);
-queue.add(20);
-queue.add(30);
-queue.add(40);
-queue.add(50);
-queue.add(60);
+// queue.add(10);
+// queue.add(20);
+// queue.add(30);
+// queue.add(40);
+// queue.add(50);
+// queue.add(60);
+//
+// console.log(queue.isEmpty())
+// console.log(queue.front()) // returns 10
+// console.log(queue.remove())
+// console.log(queue.printQueue()) // [20, 30, 40, 50, 60]
 
-console.log(queue.isEmpty())
-console.log(queue.front()) // returns 10
-console.log(queue.remove())
-console.log(queue.printQueue()) // [20, 30, 40, 50, 60] 
+
+const pq = new PriorityQueue();
+pq.add(['Beau Carnes', 2]);
+pq.add(['Quincy Larson', 3]);
+pq.add(['Ewa Mitulska-Wójcik', 1])
+pq.add(['Briana Swift', 2])
+pq.printQueue();
+// pq.remove();
+// console.log(pq.front());
+// pq.printQueue();
