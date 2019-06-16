777a684663a6db5f55d92f7656872efbd121d5c9
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-06-16 00:23:27 -0400

    Change file name and refactor

--- javascript/dataStructure/hashTable.js
+++ javascript/dataStructure/hashTable.js
@@ -1,44 +1,45 @@
 'use strict'
 const hash = (str, max) => {
-  const hash = 0
+  let hash = 0
   for (let i = 0; i < str.length; i++) {
-    hash += str.charCcodeAt(i)
+    hash += str.charCodeAt(i)
   }
   return hash % max
 }
 
 class HashTable {
   constructor() {
-    this.storage = {}
-    this.storageLimit = 4
+    this.storage = []
+    this.storageLimit = 14
   }
   print() {
     console.log(this.storage)
   }
   add(key, value) {
     const index = hash(key, this.storageLimit)
-    if (storage[index] === undefined) {
-      storage[index] = [key, value]
+    // console.log(index)
+    if (this.storage[index] === undefined) {
+      this.storage[index] = [[key, value]]
     } else {
       let added = false
-      for(let i = 0; i < this.storage[index].length; i++){
-        if (this.storage[index][i][0]) === key {
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
           this.storage[index][i][1] = value
           added = true
         }
       }
-      if(added === false) {
-        storage[index].push(key,value)
+      if (added === false) {
+        this.storage[index].push([key, value])
       }
     }
   }
 
   remove(key) {
     const index = hash(key, this.storageLimit)
-    if(this.storage[index].length === 1 && this.storage[index][0][0] === key) {
-      delete storage[index]
+    if (this.storage[index].length === 1 && this.storage[index][0][0] === key) {
+      delete this.storage[index]
     } else {
-      for(let i = 0; i < this.storage[index].length; i++) {
+      for (let i = 0; i < this.storage[index].length; i++) {
         if (this.storage[index][i][0] === key) {
           delete this.storage[index][i]
         }
@@ -59,3 +60,13 @@ class HashTable {
     }
   }
 }
+
+console.log(hash('quincy', 10)) // 5
+
+let ht = new HashTable();
+ht.add('beau', 'person');
+ht.add('fido', 'dog');
+ht.add('rex', 'dinosour');
+ht.add('tux', 'penguin')
+console.log(ht.lookup('tux'))
+ht.print();
