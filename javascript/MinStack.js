25669117c34b12fe7bbf27317e8ea5591b4bbff0
Phuongnhat Nguyen <pnguyen2@novetta.com>
2021-11-14 23:48:29 -0500

    Update MinStack code solution

--- javascript/MinStack.js
+++ javascript/MinStack.js
@@ -11,7 +11,7 @@
  * initialize your data structure here.
  */
 var MinStack = function() {
-    this.items = [];
+    this.stack = [];
 };
 
 /*
@@ -19,28 +19,28 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(x) {
-  this.items.push(x)
+  this.stack[this.stack.length] = x
 };
 
 /*
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-  this.items.pop()
+  this.stack.splice(this.stack.length - 1)
 };
 
 /*
  * @return {number}
  */
 MinStack.prototype.top = function() {
-  return this.items[this.items.length - 1]
+  return this.stack[this.stack.length - 1]
 };
 
 /*
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-  return Math.min.apply(null, this.items )
+  return Math.min.apply(null, this.stack )
 };
 
 /**
