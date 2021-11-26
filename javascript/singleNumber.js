c15c3d8b3dbb1db4dd4f7632a344acbdab1a5982
Phuongnhat Nguyen <pnguyen2@novetta.com>
2021-11-26 18:17:57 -0500

    Add new code solutions to "Single Number" code challenge.

--- javascript/singleNumber.js
+++ javascript/singleNumber.js
@@ -1,3 +1,5 @@
+// 136. Single Number
+
 // Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 //
 // Note:
@@ -5,15 +7,28 @@
 // Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 //
 // Example 1:
-//
-// Input: [2,2,1]
+
+// Input: nums = [2,2,1]
 // Output: 1
 // Example 2:
-//
-// Input: [4,1,2,1,2]
+
+// Input: nums = [4,1,2,1,2]
 // Output: 4
+// Example 3:
 
+// Input: nums = [1]
+// Output: 1
 
+// Constraints:
+
+// 1 <= nums.length <= 3 * 104
+// -3 * 104 <= nums[i] <= 3 * 104
+// Each element in the array appears twice except for one element which appears only once.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 // function singleNumber(nums) {
 //   for (let i = 0; i < nums.length; i++) {
 //       const first = nums.indexOf(nums[i])
@@ -24,19 +39,57 @@
 //   }
 // }
 
-// // alternative solution
+// Alternative solution
+// function singleNumber(nums) {
+//   const numbers = {};
+//   for (let i = 0; i < nums.length; i++) {
+//     if (!numbers[nums[i]]) {
+//       numbers[nums[i]] = true;
+//     } else {
+//       delete numbers[nums[i]];
+//     }
+//   }
+//   return Number(Object.keys(numbers)[0]);
+// }
+
+// function singleNumber(nums) {
+//   const hash = nums.reduce((acc, num) => {
+//     if (acc[num]) {
+//       acc[num]++;
+//     } else {
+//       acc[num] = 1;
+//     }
+//     return acc;
+//   }, {});
+
+//   for (const num of Object.keys(hash)) {
+//     if (hash[num] === 1) return Number(num);
+//   }
+// }
+
 function singleNumber(nums) {
-  const numbers = {}
-  for (let i = 0; i < nums.length; i++) {
-    if (!numbers[nums[i]]) {
-      numbers[nums[i]] = true
-    } else {
-      delete numbers[nums[i]]
-    }
-  }
-  return Object.keys(numbers)[0]
+  return nums.reduce((pre, curr) => pre ^ curr )
 }
 
+const testCases = [
+  {
+    input: [2, 2, 1],
+    expected: 1,
+  },
+  {
+    input: [4, 1, 2, 1, 2],
+    expected: 4,
+  },
+  {
+    input: [1],
+    expected: 1,
+  },
+];
 
-console.log(singleNumber([2,2,1])) // 1
-console.log(singleNumber([4,1,2,1,2])) // 4
+for (const [i, test] of testCases.entries()) {
+  const { input, expected } = test;
+  const result = singleNumber(input);
+  if (result !== expected) {
+    console.log(`case ${i} failed, expected: ${expected}, actual: ${result}`);
+  }
+}
