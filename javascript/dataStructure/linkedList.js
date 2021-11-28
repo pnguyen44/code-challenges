e904798e481bea59d2bb976b2e436038da40310d
Phuongnhat Nguyen <pnguyen2@novetta.com>
2021-11-27 22:29:27 -0500

    Update linkedList implementation

--- javascript/dataStructure/linkedList.js
+++ javascript/dataStructure/linkedList.js
@@ -1,155 +1,168 @@
-// implement a linked list with a insert, append, prepend, and printList method
-
+// implement a linked list with a insert, insertAt, removeFrom, removeData, reverseList, indexOf, toArray, and printList method
 
 class Node {
-  constructor (data) {
-    this.data = data
-    this.next = null
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
 }
-
 
 class LinkedList {
   constructor() {
-    this.head = null,
-    this.size = 0
+    this.head = null;
+    this.size = 0;
   }
 
   insert(data) {
-    // create a new node
-    let node = new Node(data)
-    let current
-    if (this.head == null) {
-      this.head = node
-    } else {
-      current = this.head
-      while(current.next) {
-        current = current.next
-      }
-      // add node
-      current.next = node
+    const node = new Node(data);
+
+    if (!this.head) {
+      this.head = node;
+      return;
     }
-    this.size++
+
+    let curr = this.head;
+
+    while (curr.next) {
+      curr = curr.next;
+    }
+
+    curr.next = node;
+    this.size++;
   }
 
   insertAt(data, index) {
-    if (index > 0 && index > this.size) {
-      return false
-    } else {
-      const newNode = new Node(data)
-      let curr, prev
-      curr = this.head
-      if (index == 0) {
-        newNode.next = this.head
-        this.head = newNode
-      } else {
-        curr = this.head
-        let i = 0
-        while (i < index) {
-          i++
-          prev = curr
-          curr = curr.next
-        }
-        // add element
-        newNode.next = curr
-        prev.next = newNode
-      }
-      this.size++
+    if (index < 0 || index > this.size) {
+      console.log("invalid index");
+      return;
     }
+
+    let curr = this.head;
+    let prev = null;
+    const node = new Node(data);
+
+    if (index === 0) {
+      node.next = this.head;
+      this.head = node;
+      return;
+    }
+
+    let i = 0;
+    while (i < index) {
+      i++;
+      prev = curr;
+      curr = curr.next;
+    }
+    prev.next = node;
+    node.next = curr;
+    this.size++;
   }
 
   removeFrom(index) {
-     if (index > 0 && index > this.size) {
-       return -1
-     } else {
-       let curr, prev, i = 0
-       curr = this.head
-       prev = curr
-       if (index == 0) {
-         this.head = curr.next
-       } else {
-         while(i < index) {
-           i++
-           prev = curr
-           curr = curr.next
-         }
-         // remove element
-         prev.next = curr.next
-       }
-      this.size--
-      return curr.data
-   }
- }
-
- removeData(data) {
-   let curr = this.head
-   let prev = null
-   while (curr != null) {
-     if(curr.data === data) {
-       if(prev == null) {
-         this.head = curr.next
-       } else {
-         prev.next = curr.next
-       }
-       this.size--
-       return curr.data
-     }
-     prev = curr
-     curr = curr.next
-   }
-   return -1
- }
-
-
-  reverseList(head = this.head) {
-    let prev = null
-    let curr = this.head
-    while(curr != null) {
-      let next = curr.next
-      curr.next = prev
-      prev = curr
-      curr = next
+    if (index < 0 || index > this.size) {
+      console.log("invalid index");
+      return null;
     }
-    this.head = prev
+    let i = 0;
+    let prev = null;
+    let curr = this.head;
+
+    if (index === 0) {
+      this.head = curr.next;
+    } else {
+      while (i < index) {
+        i++;
+        prev = curr;
+        curr = curr.next;
+      }
+
+      prev.next = curr.next;
+    }
+
+    this.size--;
+    return curr.data;
+  }
+
+  removeData(data) {
+    let curr = this.head;
+    let prev = null;
+
+    while (curr) {
+      if (curr.data === data) {
+        if (!prev) {
+          this.head = curr.next;
+        } else {
+          prev.next = curr.next;
+        }
+        this.size--;
+        return curr.data;
+      }
+      prev = curr;
+      curr = curr.next;
+    }
+    return -1;
+  }
+
+  reverseList() {
+    let curr = this.head;
+    let prev = null;
+    while (curr) {
+      const next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    this.head = prev;
   }
 
   indexOf(data) {
-    let count = 0
-    let curr = this.head
-    while(curr != null) {
-      if(curr.data === data) {
-        return count
-      } else {
-        count++
-        curr = curr.next
+    let curr = this.head;
+    let index = 0;
+    while (curr) {
+      if (curr.data === data) {
+        return index;
       }
+      index++;
+      curr = curr.next;
     }
-    // not found
-    return -1
+    return -1;
+  }
+
+  toArray() {
+    const nodes = [];
+    let curr = this.head;
+    while (curr) {
+      nodes.push(curr.data);
+      curr = curr.next;
+    }
+    return nodes;
   }
 
   printList() {
-    let current = this.head
-    let str = ''
-    while(current) {
-      str += current.data + ' '
-      current = current.next
-    }
-    console.log(str)
+    const nodes = this.toArray();
+    console.log(nodes.join(" "));
   }
 }
 
-let ll = new LinkedList()
+const ll = new LinkedList();
 ll.insert(10);
 ll.insert(20);
 ll.insert(30);
 ll.insert(40);
 ll.insert(50);
-console.log('data removed', ll.removeData(50))
-ll.printList() // 10 20 30 40
-
-// console.log(ll.indexOf(40)) // 3
-ll.insertAt(60,2)
-ll.printList() // 10 20 60 30 40
-console.log(ll.removeFrom(3))
+ll.printList(); // 10 20 30 40 50
+console.log("data removed", ll.removeData(50)); // data removed 50
+ll.printList(); // 10 20 30 40
+console.log("data removed", ll.removeData(50)); // data removed -1
+console.log(ll.indexOf(40)); // 3
+console.log(ll.indexOf(50)); // -1
+ll.insertAt(60, 2);
+ll.printList(); // 10 20 60 30 40
+ll.insertAt(60, -1); // invalid index
+ll.insertAt(60, 10); // invalid index
+console.log("data removed", ll.removeFrom(3));
 ll.printList(); // 10 20 60 40
+console.log(ll.removeFrom(99)); // invalid index
+ll.reverseList();
+ll.printList(); // 40 60 20 10
+console.log(`size=${ll.size}`); // size=4
