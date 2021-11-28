c4c8e63f90ed5da7c7315cf305257ab7941c9c31
Phuongnhat Nguyen <pnguyen2@novetta.com>
2021-11-28 14:18:17 -0500

    Update code solution to "Merge Two Sorted Lists" code challenge.

--- javascript/dataStructure/mergeTwoLists.js
+++ javascript/dataStructure/mergeTwoLists.js
@@ -1,75 +1,103 @@
- // Merge Two Sorted Lists
+// 21. Merge Two Sorted Lists
 
- // Merge two sorted linked lists and return it as a new list.
- // The new list should be made by splicing together the nodes of the first two lists.
+// You are given the heads of two sorted linked lists list1 and list2.
 
-// Example:
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 
-// Input: 1->2->4, 1->3->4
-// Output: 1->1->2->3->4->4
+// Return the head of the merged linked list.
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
+// Example 1:
+
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// Example 2:
+
+// Input: list1 = [], list2 = []
+// Output: []
+// Example 3:
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+// Constraints:
+
+// The number of nodes in both lists is in the range [0, 50].
+// -100 <= Node.val <= 100
+// Both list1 and list2 are sorted in non-decreasing order.
+
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 
- class Node {
-   constructor(val) {
-     this.val = val
-     this.next = null
-   }
- }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
 
- class LinkedList {
-   constructor() {
-     this.head = null
-     this.size = 0
-   }
-   add(val) {
-     const newNode = new Node(val)
-     let curr
-     if (this.head === null) {
-       this.head = newNode
-     } else {
-       curr = this.head
-       while(curr.next != null) {
-         curr = curr.next
-       }
-       curr.next = newNode
-     }
-   }
- }
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
- const l1 = new LinkedList()
- const l2 = new LinkedList()
- l1.add(1)
- l1.add(2)
- l1.add(4)
- l2.add(1)
- l2.add(3)
- l2.add(4)
+  add(val) {
+    const newNode = new Node(val);
 
- var mergeTwoLists = function(l1, l2) {
-   let mergedLinkedListHead = { val : -1, next : null }; // create dummy node to get started
-   let curr = mergedLinkedListHead;
-   while(l1 && l2) {
-     if(l1.val < l2.val) {
-       curr.next = l1
-       l1 = l1.next
-     } else {
-       curr.next = l2
-       l2 = l2.next
-     }
-     curr = runner.next;
-   }
-   curr.next = l1 || l2;
-   return mergedLinkedListHead.next
- };
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let curr = this.head;
+    while (curr.next) {
+      curr = curr.next;
+    }
+
+    curr.next = newNode;
+  }
+}
+
+function toArray(linkedList) {
+  let curr = linkedList.head;
+  let nodes = [];
+
+  while (curr) {
+    nodes.push(curr.val);
+    curr = curr.next;
+  }
+
+  return nodes;
+}
+
+const l1 = new LinkedList();
+const l2 = new LinkedList();
+l1.add(1);
+l1.add(2);
+l1.add(4);
+l2.add(1);
+l2.add(3);
+l2.add(4);
+
+console.log("l1 =", toArray(l1)); // l1 = [ 1, 2, 4 ]
+console.log("l2 =", toArray(l2)); // l2 = [ 1, 3, 4 ]
+
+var mergeTwoLists = function (list1, list2) {
+  const mergedLinkedListHead = { val: -1, next: null }; // create dummy node to get started
+  let curr = mergedLinkedListHead;
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      curr.next = list1;
+      list1 = list1.next;
+    } else {
+      curr.next = list2;
+      list2 = list2.next;
+    }
+    curr = curr.next;
+  }
+  curr.next = list1 || list2;
+  return mergedLinkedListHead.next;
+};
